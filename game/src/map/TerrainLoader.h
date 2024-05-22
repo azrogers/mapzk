b2ceb3a0413959b5ee13bca96fbf8ce7a2126a73
Ashley Rogers <ashley@anime.lgbt>
2024-05-22 15:08:55 -0400

    Remove c++ project

--- game/src/map/TerrainLoader.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <vsg/all.h>
-
-#include "../util/ContentLoader.h"
-
-class TerrainLoader {
-	static vsg::ref_ptr<vsg::Node> Load(const std::string& mapDefPath);
-};
--- lib/clauser/src/clauser/ClUtil.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define CL_WRAP_CALL(FunctionCall)                                                                           \
-    if (!FunctionCall) {                                                                                     \
-        status.GetError().Log();                                                                             \
-        return false;                                                                                        \
-    }
